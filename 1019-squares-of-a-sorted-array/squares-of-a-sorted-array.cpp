e48b8a9f64b0b1ad2b2281d24e5fbf3bbda656bb
Krishna Singh Sikarwar <krishnaasingh991@gmail.com>
2025-08-19 15:29:48 +0530

    Time: 4 ms (47.32%) | Memory: 30.1 MB (98.17%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.cpp
@@ -7,16 +7,7 @@ public:
         {
             nums[i]=pow(nums[i],2);
         }
-        for(int i=0;i<n-1;i++)
-        {
-            for(int j=0;j<n-i-1;j++)
-            {
-                if(nums[j]>nums[j+1])
-                {
-                    swap(nums[j],nums[j+1]);
-                }
-            }
-        }
+        sort(nums.begin(),nums.end());
         return nums;
     }
 };
