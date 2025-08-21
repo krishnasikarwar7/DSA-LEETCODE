0728daece6dce8ca825342ab4967717298659abd
Krishna Singh Sikarwar <krishnaasingh991@gmail.com>
2025-08-21 12:53:59 +0530

    Time: 3 ms (18.12%) | Memory: 7.9 MB (37.57%) - LeetSync

---
+++ 278-first-bad-version/first-bad-version.cpp
@@ -0,0 +1,27 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int ans=-1;
+        long low=1,high=n;
+        long long mid;
+
+        while(low<=high)
+        {
+            mid=(low+high)/2;
+
+            if(isBadVersion(mid))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
+            }
+        }
+        return ans;
+    }
+};
