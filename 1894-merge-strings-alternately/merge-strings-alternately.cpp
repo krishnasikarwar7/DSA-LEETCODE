b789c433533b8bec25a18392b1bc0dc5159c6e63
Krishna Singh Sikarwar <krishnaasingh991@gmail.com>
2025-08-24 23:45:48 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (31.88%) - LeetSync

---
+++ 1894-merge-strings-alternately/merge-strings-alternately.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int m=word1.length();
+        int n=word2.length();
+        string merged;
+
+        int i=0,j=0;
+
+        while(i<m || j<n)
+        {
+            if(i<m)
+            {
+                merged.push_back(word1[i++]);
+            }
+            if(j<n)
+            {
+                merged.push_back(word2[j++]);
+            }
+        }
+        
+        return merged;
+    }
+};
