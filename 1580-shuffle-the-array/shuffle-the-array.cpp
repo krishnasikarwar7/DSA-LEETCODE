3f99767eeac9e2b33391cf4a2a4d52207a37039b
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2025-09-01 22:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (83.00%) - LeetSync

--- 1580-shuffle-the-array/shuffle-the-array.cpp
+++ 1580-shuffle-the-array/shuffle-the-array.cpp
@@ -1,25 +1,13 @@
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
-        n=nums.size();
-        vector<int> arr1;
-        vector<int> arr2;
 
         vector<int> ans;
 
-        for(int i=0;i<(n/2);i++)
+        for(int i=0;i<n;i++)
         {
-            arr1.push_back(nums[i]);
-        }
-        for(int i=(n/2);i<n;i++)
-        {
-            arr2.push_back(nums[i]);
-        }
-
-        for(int i=0;i<(n/2);i++)
-        {
-            ans.push_back(arr1[i]);
-            ans.push_back(arr2[i]);
+            ans.push_back(nums[i]);
+            ans.push_back(nums[i+n]);
         }
 
         return ans;
