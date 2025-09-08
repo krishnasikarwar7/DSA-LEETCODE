a49c5c820e22192fbc42acf6a147767eb0cc280d
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2025-09-08 15:03:18 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (67.61%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -6,10 +6,6 @@ public:
 
         for(int i=0;i<n;i++)
         {
-            ans*=nums[i];
-        }
-        for(int i=0;i<n;i++)
-        {
             ans^=nums[i];
         }
         return ans;
