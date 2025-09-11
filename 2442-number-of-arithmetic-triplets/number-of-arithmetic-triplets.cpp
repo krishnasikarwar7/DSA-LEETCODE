6ca3772244c33b23d18e5e2b0af7fa01f1c56ac1
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2025-09-11 22:17:41 +0530

    Time: 3 ms (49.82%) | Memory: 12 MB (36.85%) - LeetSync

--- 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
+++ 2442-number-of-arithmetic-triplets/number-of-arithmetic-triplets.cpp
@@ -3,17 +3,13 @@ public:
     int arithmeticTriplets(vector<int>& nums, int diff) {
 
         int c=0;
-        for(int i=0;i<nums.size();i++)
+        unordered_set<int> m(nums.begin(),nums.end());
+
+        for(int x:nums)
         {
-            for(int j=i+1;j<nums.size();j++)
+            if(m.count(x+diff) && m.count(x+ 2*diff))
             {
-                for(int k=j+1;k<nums.size();k++)
-                {
-                    if(nums[j] - nums[i] == diff && nums[k] - nums[j] == diff)
-                    {
-                        c++;
-                    }
-                }
+                c++;
             }
         }
        
