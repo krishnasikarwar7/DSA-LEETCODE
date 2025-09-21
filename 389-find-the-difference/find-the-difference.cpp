efeb6f2e52eaddffe58de0061d3955723f602f81
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2025-09-21 22:30:59 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (52.18%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    char findTheDifference(string s, string t) {
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        
+        int m=t.size();
+
+        for(int i=0;i<m;i++)
+        {
+            if(s[i]!=t[i])
+            {
+                return {t[i]};
+            }
+        }
+        
+        return {};
+    }
+};
