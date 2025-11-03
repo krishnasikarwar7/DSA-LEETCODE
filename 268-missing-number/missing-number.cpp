2c2b8eae812710aad0430a0553f8e015592c70c6
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2025-11-03 22:45:14 +0530

    Time: 0 ms (100.00%) | Memory: 21.7 MB (92.42%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,19 +1,9 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_map<int,int> freq;
         int n=nums.size();
-
-        for(int i=0;i<n;i++)
-        {
-            freq[nums[i]]++;
-        }
-
-        for (int i = 0; i <= n; i++) {
-            if (freq.find(i) == freq.end())
-                return i;
-        }
-
-        return -1;
+        int sum=(n*(n+1))/2;
+        int real=accumulate(nums.begin(),nums.end(),0);
+        return sum-real;
     }
 };
