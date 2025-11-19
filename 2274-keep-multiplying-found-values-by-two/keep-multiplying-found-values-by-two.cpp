bb944bc8cb36be6bc36c7fafb2767b087129d8d9
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2025-11-19 07:02:07 +0530

    Time: 4 ms (31.07%) | Memory: 15.7 MB (32.40%) - LeetSync

---
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        unordered_set<int> st(nums.begin(),nums.end());
+
+        while(st.find(original)!=st.end())
+        {
+            original*=2;
+        }
+
+        return original;
+    }
+};
