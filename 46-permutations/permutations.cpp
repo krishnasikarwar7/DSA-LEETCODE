4d2d02b7c401c8fdddfc1cb8d95c624ed4b5a480
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2025-12-02 21:32:56 +0530

    Time: 1 ms (42.56%) | Memory: 10.5 MB (69.84%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void backtrk(vector<int>&nums,vector<int>&curr,vector<bool>&used,vector<vector<int>>&ans)
+    {
+        if(curr.size()==nums.size())
+        {
+            ans.push_back(curr);
+            return;
+        }
+        for(int i=0;i<nums.size();i++)
+        {
+            if(used[i])
+                continue;
+            
+            used[i]=true;
+            curr.push_back(nums[i]);
+            backtrk(nums,curr,used,ans);
+            curr.pop_back();
+            used[i]=false;
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<int> curr;
+        vector<bool> used(nums.size(),false);
+        vector<vector<int>> ans;
+        backtrk(nums,curr,used,ans);
+        return ans;
+    }
+};
