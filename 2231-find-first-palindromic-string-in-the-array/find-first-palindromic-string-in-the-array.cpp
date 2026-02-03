3c50144427f777fc21aa4d67bcbf28f66cafa112
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2026-02-03 22:08:22 +0530

    Time: 11 ms (21.33%) | Memory: 29.8 MB (24.03%) - LeetSync

---
+++ 2231-find-first-palindromic-string-in-the-array/find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+        for(string s:words)
+        {
+            string rev=s;
+            reverse(rev.begin(),rev.end());
+            if(s==rev)
+                return s;
+        }
+        return "";
+    }
+};
