8c684cc901abea2dd7f25e9219a7b2b597f91a6c
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2026-02-06 22:36:12 +0530

    Time: 3 ms (29.08%) | Memory: 52.5 MB (18.61%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int r=0;
+        int n=nums.size();
+
+        for(int i=0;i<n;i++)
+        {
+            if(i>r) return false;
+            r=max(r,i+nums[i]);
+            if(r>=(n-1)) return true;
+        }
+
+        return true;
+
+    }
+};
