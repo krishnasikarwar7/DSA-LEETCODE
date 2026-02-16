1cdd7b4f6e09a951f4a499b2866cd1db50ad7b62
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2026-02-16 14:04:56 +0530

    Time: 0 ms (100.00%) | Memory: 10.7 MB (41.83%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
-    void backtrk(vector<int>&nums,vector<int>&curr,vector<bool>&used,vector<vector<int>>&ans)
+    void backtr(vector<int>& nums,vector<int>& newa, vector<vector<int>>& ans, vector<bool>& freq)
     {
-        if(curr.size()==nums.size())
+        if(newa.size()==nums.size())
         {
-            ans.push_back(curr);
+            ans.push_back(newa);
             return;
         }
+
         for(int i=0;i<nums.size();i++)
         {
-            if(used[i])
-                continue;
-            
-            used[i]=true;
-            curr.push_back(nums[i]);
-            backtrk(nums,curr,used,ans);
-            curr.pop_back();
-            used[i]=false;
+            if(!freq[i])
+            {
+                freq[i]=true;
+                newa.push_back(nums[i]);
+                backtr(nums,newa,ans,freq);
+                newa.pop_back();
+                freq[i]=false;
+            }
         }
     }
+
     vector<vector<int>> permute(vector<int>& nums) {
-        vector<int> curr;
-        vector<bool> used(nums.size(),false);
+        vector<int> newa;
         vector<vector<int>> ans;
-        backtrk(nums,curr,used,ans);
+        vector<bool> freq(nums.size(),false);
+        backtr(nums,newa,ans,freq);
         return ans;
     }
 };
