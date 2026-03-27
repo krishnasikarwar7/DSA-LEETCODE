55c17528d131c8ccc5e03eb8a6b76b47f9ee1353
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2026-03-27 22:43:40 +0530

    Time: 5 ms (28.96%) | Memory: 23.4 MB (66.93%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -1,50 +1,40 @@
-#include <stack>
-using namespace std;
-
 class MinStack {
 private:
-    stack<long long> st;   
-    long long mini;
-
+    stack<int> st;
+    stack<int> minst;
 public:
-    MinStack() {}
-
+    MinStack() {
+        
+    }
+    
     void push(int val) {
-        if (st.empty()) {
-            st.push(val);
-            mini = val;
-        }
-        else if (val >= mini) {
-            st.push(val);
-        }
-        else {
-            long long encoded = 2LL * val - mini;
-            st.push(encoded);
-            mini = val;
-        }
+        st.push(val);
+        if(minst.empty()) minst.push(val);
+        else minst.push(min(val,minst.top()));
     }
-
+    
     void pop() {
-        if (st.empty()) return;
-
-        long long topVal = st.top();
-        st.pop();
-
-        if (topVal < mini) {
-            mini = 2LL * mini - topVal;
+        if(!st.empty())
+        {
+            st.pop();
+            minst.pop();
         }
     }
-
+    
     int top() {
-        long long topVal = st.top();
-
-        if (topVal >= mini)
-            return (int)topVal;
-        else
-                return (int)mini;  
+        return st.top();
     }
-
+    
     int getMin() {
-        return (int)mini;
+        return minst.top();
     }
 };
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
