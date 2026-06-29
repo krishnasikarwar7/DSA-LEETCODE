a195cef9dac8a02bb3f3f84cd700494f4e337b6d
Krishna Singh Sikarwar <176809250+krishnasikarwar7@users.noreply.github.com>
2026-06-29 15:50:50 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (12.95%) - LeetSync

---
+++ 1127-last-stone-weight/last-stone-weight.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        int n=stones.size();
+        priority_queue<int> pq;
+        for(int& s:stones)
+        {
+            pq.push(s);
+        }
+
+        while(pq.size()>1)
+        {
+            int x=pq.top(); pq.pop();
+            int y=pq.top(); pq.pop();
+            int z=abs(x-y);
+            pq.push(z);
+        }
+
+        return pq.top();
+    }
+};
